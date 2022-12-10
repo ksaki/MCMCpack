b18355958ef055624e6bb280ef2183384a531914
ksaki <sakikuzushima@gmail.com>
2022-12-10 10:47:26 -0500

    activate new samplers

--- src/MCMCordfactanalExperiment.cc
+++ src/MCMCordfactanalExperiment.cc
@@ -143,55 +143,61 @@ void MCMCordfactanalExperiment_impl(rng<RNGTYPE>& stream,
     for (unsigned int i = 0; i < N; ++i) {
       // Original
       /////////////////////////////////////////////////////////////////////////
-      Matrix<> phi_post_mean = phi_post_var * (t(Lambda_rest)  
-		  		       * (t(Xstar(i,_))-Lambda_const));
+      //Matrix<> phi_post_mean = phi_post_var * (t(Lambda_rest)  
+		  // 		       * (t(Xstar(i,_))-Lambda_const));
       /////////////////////////////////////////////////////////////////////////
       // With treatment and covariates for phi
-      //Matrix<> tau_obs(1, K);
-      //for (unsigned int j = 0; j < K; ++j) {
-      //  unsigned int h = treatment(i,j);
-      //  tau_obs(1,j) = tau(i,h);
-      //}
-      //Matrix<> Lambda_treat = t(Lambda_rest) * t(tau_obs);
-      //Matrix<> phi_post_mean = phi_post_var * (t(Lambda_rest)  
-			//		       * (t(Xstar(i,_))-Lambda_const-Lambda_treat) + cov_phi(i,_) * coef_phi);
+      Matrix<> tau_obs(1, K);
+      for (unsigned int j = 0; j < K; ++j) {
+        unsigned int h = treatment(i,j);
+        tau_obs(1,j) = tau(i,h);
+      }
+      Matrix<> Lambda_treat = t(Lambda_rest) * t(tau_obs);
+      Matrix<> phi_post_mean = phi_post_var * (t(Lambda_rest)  
+					       * (t(Xstar(i,_))-Lambda_const-Lambda_treat) + cov_phi(i,_) * coef_phi);
 
-      //Matrix<> phi_samp = gaxpy(phi_post_C, stream.rnorm(D-1, 1, 0, 1), 
-			//	phi_post_mean);
-      //for (unsigned int j = 0; j < (D-1); ++j)
-	    //  phi(i,j+1) = phi_samp(j);
+      Matrix<> phi_samp = gaxpy(phi_post_C, stream.rnorm(D-1, 1, 0, 1), 
+				phi_post_mean);
+      for (unsigned int j = 0; j < (D-1); ++j)
+	      phi(i,j+1) = phi_samp(j);
       /////////////////////////////////////////////////////////////////////////
     }
 				
     // sample Lambda
     /////////////////////////////////////////////////////////////////////////
     // Original:
-    NormNormfactanal_Lambda_draw(Lambda, Lambda_free_indic, 
-				 Lambda_prior_mean, Lambda_prior_prec,
-				 phi, Xstar, Psi_inv, Lambda_ineq, D, K,
-				 stream);
+    //NormNormfactanal_Lambda_draw(Lambda, Lambda_free_indic, 
+		//		 Lambda_prior_mean, Lambda_prior_prec,
+		//		 phi, Xstar, Psi_inv, Lambda_ineq, D, K,
+		//		 stream);
     /////////////////////////////////////////////////////////////////////////
     // New scheme:
     // make a copy and fill?
     // This is pretty memory inefficient. Think about how to make it more efficient
-    //for (unsigned int j = 0; j < K; ++j){
-    //  Matrix<> Lambda_row = Lambda(j,_);
-    //  Matrix<> Lambda_free_indic_row = Lambda_free_indic(j,_);
-    //  Matrix<> Lambda_prior_mean_row = Lambda_prior_mean(j,_);
-    //  Matrix<> Lambda_prior_prec_row = Lambda_prior_prec(j,_);
-    //  Matrix<> phi_obs = phi;
-    //  for (unsigned int i = 0; i < N; ++i){
-    //    unsigned int h = treatment(i,j);
-    //    phi_obs(i,0) += tau(i,h);
-    //  }
-    //  Matrix<> Xstar_row = Xstar(_,j);
-    //  Matrix<> Lambda_ineq_row = Lambda_ineq(j,_);
-    //  NormNormfactanal_Lambda_draw(Lambda_row, Lambda_free_indic_row, 
-    //       Lambda_prior_mean_row, Lambda_prior_prec_row,
-    //       phi_obs, Xstar_row, Psi_inv, Lambda_ineq_row, D, 1, // set K = 1
-    //       stream);
-    //  Lambda(j,_) = Lambda_row;
-    //}
+    for (unsigned int j = 0; j < K; ++j){
+      Matrix<> Lambda_row = Lambda(j,_);
+      Matrix<> Lambda_free_indic_row = Lambda_free_indic(j,_);
+      Matrix<> Lambda_prior_mean_row = Lambda_prior_mean(j,_);
+      Matrix<> Lambda_prior_prec_row = Lambda_prior_prec(j,_);
+      Matrix<> phi_obs = phi;
+      for (unsigned int i = 0; i < N; ++i){
+        unsigned int h = treatment(i,j);
+        phi_obs(i,0) += tau(i,h);
+      }
+      Matrix<> Xstar_row = Xstar(_,j);
+      Matrix<> Lambda_ineq_row = Lambda_ineq(j,_);
+      NormNormfactanal_Lambda_draw(Lambda_row, Lambda_free_indic_row, 
+           Lambda_prior_mean_row, Lambda_prior_prec_row,
+           phi_obs, Xstar_row, Psi_inv, Lambda_ineq_row, D, 1, // set K = 1
+           stream);
+      Lambda(j,_) = Lambda_row;
+    }
+    /////////////////////////////////////////////////////////////////////////
+
+
+    /////////////////////////////////////////////////////////////////////////
+    // XX: New sections to be added:
+    // Sample tau, eta (coef of the mean of tau) etc...
     /////////////////////////////////////////////////////////////////////////
 
     // sample gamma
@@ -215,50 +221,50 @@ void MCMCordfactanalExperiment_impl(rng<RNGTYPE>& stream,
       // loop over observations and construct the acceptance ratio
       // ///////////////////////////////////////////////////////////////////////
       // Original
-      for (unsigned int i = 0; i < N; ++i) {
-        if (X(i,j) != -999) {
-          if (X(i,j) == ncateg(j)) {
-            loglikerat = loglikerat + 
-              log(1.0  - pnorm(gamma_p[X(i,j)-1] - X_mean[i], 0, 1) ) 
-              - log(1.0 - pnorm(gamma(X(i,j)-1,j) - X_mean[i], 0, 1) );
-          } else if (X(i,j) == 1) { 
-            loglikerat = loglikerat + 
-              log(pnorm(gamma_p[X(i,j)] - X_mean[i], 0, 1)  ) 
-              - log(pnorm(gamma(X(i,j), j) - X_mean[i], 0, 1) );
-          } else { 
-            loglikerat = loglikerat + 
-              log(pnorm(gamma_p[X(i,j)] - X_mean[i], 0, 1) 
-            - pnorm(gamma_p[X(i,j)-1] - X_mean[i], 0, 1) ) 
-              - log(pnorm(gamma(X(i,j), j) - X_mean[i], 0, 1) - 
-              pnorm(gamma(X(i,j)-1, j) - X_mean[i], 0, 1) ); 
-          }
-        }
-      }
+      //for (unsigned int i = 0; i < N; ++i) {
+      //  if (X(i,j) != -999) {
+      //    if (X(i,j) == ncateg(j)) {
+      //      loglikerat = loglikerat + 
+      //        log(1.0  - pnorm(gamma_p[X(i,j)-1] - X_mean[i], 0, 1) ) 
+      //        - log(1.0 - pnorm(gamma(X(i,j)-1,j) - X_mean[i], 0, 1) );
+      //    } else if (X(i,j) == 1) { 
+      //      loglikerat = loglikerat + 
+      //        log(pnorm(gamma_p[X(i,j)] - X_mean[i], 0, 1)  ) 
+      //        - log(pnorm(gamma(X(i,j), j) - X_mean[i], 0, 1) );
+      //    } else { 
+      //      loglikerat = loglikerat + 
+      //        log(pnorm(gamma_p[X(i,j)] - X_mean[i], 0, 1) 
+      //      - pnorm(gamma_p[X(i,j)-1] - X_mean[i], 0, 1) ) 
+      //        - log(pnorm(gamma(X(i,j), j) - X_mean[i], 0, 1) - 
+      //        pnorm(gamma(X(i,j)-1, j) - X_mean[i], 0, 1) ); 
+      //    }
+      //  }
+      //}
       //
       // ///////////////////////////////////////////////////////////////////////
       // To adjust treatment effect, 
       // subtract appropriate beta_j * tau after -X_mean
-      //for (unsigned int i = 0; i < N; ++i) {
-      //  unsigned int h = treatment(i,j);
-      //  Matrix<> X_mean_treat = Lambda(j,1) * tau(i,h);
-      //  if (X(i,j) != -999) {
-      //    if (X(i,j) == ncateg(j)) {
-      //      loglikerat = loglikerat + 
-      //        log(1.0  - pnorm(gamma_p[X(i,j)-1] - X_mean[i] - X_mean_treat[0], 0, 1) ) 
-      //        - log(1.0 - pnorm(gamma(X(i,j)-1,j) - X_mean[i] - X_mean_treat[0], 0, 1) );
-      //    } else if (X(i,j) == 1) { 
-      //      loglikerat = loglikerat + 
-      //        log(pnorm(gamma_p[X(i,j)] - X_mean[i] - X_mean_treat[0], 0, 1)  ) 
-      //        - log(pnorm(gamma(X(i,j), j) - X_mean[i] - X_mean_treat[0], 0, 1) );
-      //    } else { 
-      //      loglikerat = loglikerat + 
-      //        log(pnorm(gamma_p[X(i,j)] - X_mean[i] - X_mean_treat[0], 0, 1) 
-      //      - pnorm(gamma_p[X(i,j)-1] - X_mean[i] - X_mean_treat[0], 0, 1) ) 
-      //        - log(pnorm(gamma(X(i,j), j) - X_mean[i] - X_mean_treat[0], 0, 1) - 
-      //        pnorm(gamma(X(i,j)-1, j) - X_mean[i] - X_mean_treat[0], 0, 1) ); 
-      //    }
-      //  }
-      //}
+      for (unsigned int i = 0; i < N; ++i) {
+        unsigned int h = treatment(i,j);
+        Matrix<> X_mean_treat = Lambda(j,1) * tau(i,h);
+        if (X(i,j) != -999) {
+          if (X(i,j) == ncateg(j)) {
+            loglikerat = loglikerat + 
+              log(1.0  - pnorm(gamma_p[X(i,j)-1] - X_mean[i] - X_mean_treat[0], 0, 1) ) 
+              - log(1.0 - pnorm(gamma(X(i,j)-1,j) - X_mean[i] - X_mean_treat[0], 0, 1) );
+          } else if (X(i,j) == 1) { 
+            loglikerat = loglikerat + 
+              log(pnorm(gamma_p[X(i,j)] - X_mean[i] - X_mean_treat[0], 0, 1)  ) 
+              - log(pnorm(gamma(X(i,j), j) - X_mean[i] - X_mean_treat[0], 0, 1) );
+          } else { 
+            loglikerat = loglikerat + 
+              log(pnorm(gamma_p[X(i,j)] - X_mean[i] - X_mean_treat[0], 0, 1) 
+            - pnorm(gamma_p[X(i,j)-1] - X_mean[i] - X_mean_treat[0], 0, 1) ) 
+              - log(pnorm(gamma(X(i,j), j) - X_mean[i] - X_mean_treat[0], 0, 1) - 
+              pnorm(gamma(X(i,j)-1, j) - X_mean[i] - X_mean_treat[0], 0, 1) ); 
+          }
+        }
+      }
       // ///////////////////////////////////////////////////////////////////////
 
       for (unsigned int k = 2; k < ncateg(j); ++k) {
